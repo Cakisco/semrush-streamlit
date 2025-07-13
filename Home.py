402fa0a3e7fd7ffdb726c793f3ee20395fd2a29a
Francisco <franciscodiazmayoral@gmail.com>
2025-07-13 18:23:01 +0100

    Added four classes and code to fetch datasets

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(page_title='Analytics Engineer test task', page_icon = './assets/logo.png', layout='centered')
 
@@ -12,11 +13,31 @@ The business performance for the subscription service analysed is evaluated acro
 - **Subscription products**: The relative popularity and performance of the three subscription tiers.                                            
                 """)
 
+class mrr:
+    def __init__(self, data_path):
+        self.data_path = data_path
+    def fetch_data(self):
+        self.data = pd.read_csv(self.data_path)
 
+class churn:
+    def __init__(self, data_path):
+        self.data_path = data_path
+    def fetch_data(self):
+        self.data = pd.read_csv(self.data_path)
 
+class acquisition:
+    def __init__(self, data_path):
+        self.data_path = data_path
+    def fetch_data(self):
+        self.data = pd.read_csv(self.data_path)
 
-
-
+class products:
+    def __init__(self, data_path_subs, data_path_updown):
+        self.data_path_subs = data_path_subs
+        self.data_path_updown = data_path_updown
+    def fetch_data(self):
+        self.data_subs = pd.read_csv(self.data_path_subs)
+        self.data_updown = pd.read_csv(self.data_path_updown)
 
 st.title("Analytics Engineer Test Task")
 #Part 1: Project overview
@@ -29,19 +50,32 @@ with st.container():
 #Part 2: Recurring Revenue
 st.markdown('## Recurring Revenue')
 with st.container():
+    mrr_object = mrr(data_path = './data/mrr.csv')
+    mrr_object.fetch_data()
+    st.dataframe(mrr_object.data)
     st.divider()
 
 #Part 3: Customer Retention
 st.markdown('## Customer Retention')
 with st.container():
+    churn_object = churn(data_path = './data/churn.csv')
+    churn_object.fetch_data()
+    st.dataframe(churn_object.data)
     st.divider()
 
 #Part 4: Customer Acquisition
 st.markdown('## Customer Acquisition')
 with st.container():
+    acq_object = acquisition(data_path = './data/acquisitions.csv')
+    acq_object.fetch_data()
+    st.dataframe(acq_object.data)
     st.divider()
 
 #Part 5: Subscription products
 st.markdown('## Subscription products')
 with st.container():
+    products_object = products(data_path_subs='./data/monthly_subs.csv', data_path_updown='./data/upgrades_downgrades.csv')
+    products_object.fetch_data()
+    st.dataframe(products_object.data_subs)
+    st.dataframe(products_object.data_updown)
     st.divider()
